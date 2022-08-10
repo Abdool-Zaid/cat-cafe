9eb48311d9eb110ce0ab6f5a836fe29021647431
unknown <aztoorabally7447@gmail.com>
2022-08-10 16:34:07 +0200

    'update'

--- routes/staffRoute.js
+++ routes/staffRoute.js
@@ -4,7 +4,6 @@ const con = require("../lib/db_connection");
 
 const middleware = require("../middleware/auth");
 
-
 router.get("/", (req, res) => {
   try {
     con.query("SELECT * FROM staff", (err, result) => {
@@ -37,11 +36,7 @@ router.post("/", (req, res) => {
   // if (req.header.token> 4){
 
   // the below allows you to only need one const, but every input required is inside of the brackets
-  const {
-    staffID,
-    name,
-    image,
-  } = req.body;
+  const { staffID, name, image } = req.body;
   // OR
   // the below requires you to add everything one by one
   //   const email = req.body.email;
@@ -58,48 +53,39 @@ router.post("/", (req, res) => {
     console.log(error);
     res.status(400).send(error);
   }
-// }
-// else{
-//   res.send('not admin');
-// }
+  // }
+  // else{
+  //   res.send('not admin');
+  // }
 });
 
 // update user
 
 router.put("/:id", (req, res) => {
-  if (req.body.user_type=== "._."){
+  if (req.body.user_type === "._.") {
+    try {
+      const { staffID, name, image } = req.body;
 
-  try {
-    const {
-      staffID,
-    name,
-    image,
-    } = req.body;
-
-    con.query(
-      `UPDATE staff set staffID="${staffID}",name="${name}",image="${image}" WHERE staffID = "${req.params.id}"`,
-      (err, result) => {
-        if (err) throw err;
-        res.send(result);
-      }
-    );
-  } catch (error) {
-    console.log(error);
-    res.status(400).send(error);
-
+      con.query(
+        `UPDATE staff set staffID="${staffID}",name="${name}",image="${image}" WHERE staffID = "${req.params.id}"`,
+        (err, result) => {
+          if (err) throw err;
+          res.send(result);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
+    }
+  } else {
+    res.send("not ._.");
   }
-  }
-  else{
-    res.send('not ._.');
-  }
-
 });
 
 // delete user
 
-router.delete("/:id",middleware, (req, res) => {
-  if (req.body.user_type=== "._."){
-
+router.delete("/:id", middleware, (req, res) => {
+  if (req.body.user_type === "._.") {
     try {
       con.query(
         `DELETE FROM staff WHERE staffID = "${req.params.id}" `,
@@ -107,15 +93,14 @@ router.delete("/:id",middleware, (req, res) => {
           if (err) throw err;
           res.send(result);
         }
-        );
-      } catch (error) {
-        console.log(error);
-        res.status(400).send(error);
-      }
+      );
+    } catch (error) {
+      console.log(error);
+      res.status(400).send(error);
     }
-    else{
-      res.send('not ._.');
-    }
+  } else {
+    res.send("not ._.");
+  }
 });
 
 module.exports = router;
